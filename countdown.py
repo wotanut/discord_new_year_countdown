d3c1a8f83667cba30f00cbfcb565daef560ef52b
wotanut <75843086+wotanut@users.noreply.github.com>
2021-12-31 15:58:27 +0000

    fixed typo

--- countdown.py
+++ countdown.py
@@ -11,7 +11,7 @@ RPC = Presence(client_id=client_id)
 RPC.connect()
 
 # Make sure you are using the same name that you used when uploading the image
-RPC.update(details="Time until 2022:",large_image="new_year", large_text="here's to a better 2023",end="1640995200")
+RPC.update(details="Time until 2022:",large_image="new_year", large_text="here's to a better 2022",end="1640995200")
 
 while 1:
     time.sleep(15) #Can only update presence every 15 seconds
